4c045db7a8fa91cd9aea0f303c39d6df1c9bce86
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-09-03 17:19:55 +0300

    Send custom event via EventDispatcher

--- src/Controller/Web/CreateUser/v1/Manager.php
+++ src/Controller/Web/CreateUser/v1/Manager.php
@@ -3,24 +3,23 @@
 namespace App\Controller\Web\CreateUser\v1;
 
 use App\Domain\Entity\User;
+use App\Domain\Event\CreateUserEvent;
 use App\Domain\Service\UserService;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class Manager
 {
-    public function __construct(private readonly UserService $userService)
-    {
+    public function __construct(
+        private readonly UserService $userService,
+        private readonly EventDispatcherInterface $eventDispatcher,
+    ) {
     }
 
     public function create(string $login, ?string $phone = null, ?string $email = null): ?User
     {
-        if ($phone !== null) {
-            return $this->userService->createWithPhone($login, $phone);
-        }
+        $event = new CreateUserEvent($login, $phone, $email);
+        $event = $this->eventDispatcher->dispatch($event);
 
-        if ($email !== null) {
-            return $this->userService->createWithEmail($login, $email);
-        }
-
-        return null;
+        return $event->id === null ? null : $this->userService->findUserById($event->id);
     }
 }
