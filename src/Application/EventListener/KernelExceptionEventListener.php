a3903471d69e09927c2e3d00df4b8d93e0683838
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-09-06 13:48:15 +0300

    Process validation error

--- src/Application/EventListener/KernelExceptionEventListener.php
+++ src/Application/EventListener/KernelExceptionEventListener.php
@@ -6,6 +6,9 @@ use App\Controller\Exception\HttpCompliantExceptionInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
 
 class KernelExceptionEventListener
 {
@@ -15,10 +18,20 @@ class KernelExceptionEventListener
 
         if ($exception instanceof HttpCompliantExceptionInterface) {
             $event->setResponse($this->getHttpResponse($exception->getHttpResponseBody(), $exception->getHttpCode()));
+        } elseif ($exception instanceof HttpExceptionInterface && $exception->getPrevious() instanceof ValidationFailedException) {
+            $event->setResponse($this->getValidationFailedResponse($exception->getPrevious()));
         }
     }
 
     private function getHttpResponse($message, $code): Response {
         return new JsonResponse(['message' => $message], $code);
+    }
+
+    private function getValidationFailedResponse(ValidationFailedException $exception): Response {
+        $response = [];
+        foreach ($exception->getViolations() as $violation) {
+            $response[$violation->getPropertyPath()] = $violation->getMessage();
+        }
+        return new JsonResponse($response, Response::HTTP_BAD_REQUEST);
     }
 }
