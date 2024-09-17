f984d082fd0150d4eafd6f01f4359b6cc1c8d90c
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-09-17 17:59:52 +0300

    Use JWT for authentication

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
--- src/Application/Security/AuthService.php
+++ src/Application/Security/AuthService.php
@@ -3,6 +3,8 @@
 namespace App\Application\Security;
 
 use App\Domain\Service\UserService;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTEncodeFailureException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AuthService
@@ -10,6 +12,8 @@ class AuthService
     public function __construct(
         private readonly UserService $userService,
         private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly JWTEncoderInterface $jwtEncoder,
+        private readonly int $tokenTTL,
     ) {
     }
 
@@ -23,8 +27,13 @@ class AuthService
         return $this->passwordHasher->isPasswordValid($user, $password);
     }
 
-    public function getToken(string $login): ?string
+    /**
+     * @throws JWTEncodeFailureException
+     */
+    public function getToken(string $login): string
     {
-        return $this->userService->updateUserToken($login);
+        $tokenData = ['username' => $login, 'exp' => time() + $this->tokenTTL];
+
+        return $this->jwtEncoder->encode($tokenData);
     }
 }
