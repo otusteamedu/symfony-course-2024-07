eadc809f324bc87400d7612da3d1ed1997639b82
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-10-15 15:37:28 +0300

    Add addFollowers API method

---
+++ src/Controller/Web/AddFollowers/v1/Input/AddFollowersDTO.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller\Web\AddFollowers\v1\Input;
+
+class AddFollowersDTO
+{
+    public function __construct(
+       public readonly string $followerLoginPrefix,
+       public readonly int $count,
+    ) {
+    }
+}
