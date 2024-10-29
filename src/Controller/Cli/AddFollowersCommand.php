8ec2f8c3db29cb15cb7e7fd2ad8cb62c89cbf56d
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-10-29 15:08:23 +0300

    Make the second argument optional

--- src/Controller/Cli/AddFollowersCommand.php
+++ src/Controller/Cli/AddFollowersCommand.php
@@ -11,6 +11,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 final class AddFollowersCommand extends Command
 {
+    private const DEFAULT_FOLLOWERS = 10;
+
     public function __construct(
         private readonly UserService $userService,
         private readonly FollowerService $followerService
@@ -23,25 +25,26 @@ final class AddFollowersCommand extends Command
         $this->setName('followers:add')
             ->setDescription('Adds followers to author')
             ->addArgument('authorId', InputArgument::REQUIRED, 'ID of author')
-            ->addArgument('count', InputArgument::REQUIRED, 'How many followers should be added');
+            ->addArgument('count', InputArgument::OPTIONAL, 'How many followers should be added');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $authorId = (int)$input->getArgument('authorId');
         $user = $this->userService->findUserById($authorId);
+
         if ($user === null) {
             $output->write("<error>User with ID $authorId doesn't exist</error>\n");
             return self::FAILURE;
         }
-        $count = (int)$input->getArgument('count');
+
+        $count = (int)($input->getArgument('count') ?? self::DEFAULT_FOLLOWERS);
         if ($count < 0) {
             $output->write("<error>Count should be positive integer</error>\n");
             return self::FAILURE;
         }
 
         $result = $this->followerService->addFollowersSync($user, "Reader #{$authorId}", $count);
-
         $output->write("<info>$result followers were created</info>\n");
 
         return self::SUCCESS;
