1e3e4d921fb566c54bee81d06274d3b0ee7455ff
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-11-05 17:40:12 +0300

    Fix command test

--- tests/Functional/Controller/Cli/AddFollowersCommandCest.php
+++ tests/Functional/Controller/Cli/AddFollowersCommandCest.php
@@ -25,17 +25,17 @@ class AddFollowersCommandCest
         ]);
         $params = ['authorId' => $authorId, '--login' => $example['login']];
         $inputs = $example['followersCount'] === null ? ["\n"] : [$example['followersCount']."\n"];
-        $output = $I->runSymfonyConsoleCommand(self::COMMAND, $params, $inputs);
+        $output = $I->runSymfonyConsoleCommand(self::COMMAND, $params, $inputs, $example['exitCode']);
         $I->assertStringEndsWith($example['expected'], $output);
     }
 
     protected function executeDataProvider(): array
     {
         return [
-            'positive' => ['followersCount' => 20, 'login' => 'login', 'expected' => "20 followers were created\n"],
-            'zero' => ['followersCount' => 0, 'login' => 'other_login', 'expected' => "0 followers were created\n"],
-            'default' => ['followersCount' => null, 'login' => 'login3', 'expected' => "10 followers were created\n"],
-            'negative' => ['followersCount' => -1, 'login' => 'login_too', 'expected' => "Count should be positive integer\n"],
+            'positive' => ['followersCount' => 20, 'login' => 'login', 'expected' => "20 followers were created\n", 'exitCode' => 0],
+            'zero' => ['followersCount' => 0, 'login' => 'other_login', 'expected' => "0 followers were created\n", 'exitCode' => 0],
+            'default' => ['followersCount' => null, 'login' => 'login3', 'expected' => "10 followers were created\n", 'exitCode' => 0],
+            'negative' => ['followersCount' => -1, 'login' => 'login_too', 'expected' => "Count should be positive integer\n", 'exitCode' => 1],
         ];
     }
 }
