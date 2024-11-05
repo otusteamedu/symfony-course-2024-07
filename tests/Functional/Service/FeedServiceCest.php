7aa2c8664d0c6b27f892bd0bef66de1c97b2f93d
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-11-05 18:33:54 +0300

    Fix test with associated entities

--- tests/Functional/Service/FeedServiceCest.php
+++ tests/Functional/Service/FeedServiceCest.php
@@ -51,6 +51,8 @@ class FeedServiceCest
         }
         /** @var FeedService $feedService */
         $feedService = $I->grabService(FeedService::class);
+        $I->clearEntityManager();
+        $follower = $I->grabEntityFromRepository(PhoneUser::class, ['id' => $follower->getId()]);
 
         $feed = $feedService->getFeedWithoutMaterialization($follower, $example['tweetsCount']);
 
