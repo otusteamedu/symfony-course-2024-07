b8fd32129631d90db41533290d6a54f7ad145d60
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-11-08 18:35:02 +0300

    Describe response model in controller

--- src/Controller/Web/GetFeed/v1/Controller.php
+++ src/Controller/Web/GetFeed/v1/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Web\GetFeed\v1;
 
 use App\Domain\Entity\User;
+use OpenApi\Attributes as OA;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +17,68 @@ class Controller
     public function __construct(private readonly Manager $manager) {
     }
 
+    #[OA\Get(
+        operationId: 'v1GetFeed',
+        description: 'Получение ленты для пользователя',
+        tags: ['feed'],
+        parameters: [
+            new OA\Parameter(
+                name: 'id',
+                description: 'Идентификатор пользователя',
+                in: 'path',
+                required: true,
+                schema: new OA\Schema(type: 'integer'),
+            ),
+            new OA\Parameter(
+                name: 'count',
+                description: 'Количество сообщений в ленте',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(type: 'integer'),
+            ),
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Успешный ответ',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'tweets',
+                            type: 'array',
+                            items: new OA\Items(
+                                properties: [
+                                    new OA\Property(
+                                        property: 'id',
+                                        type: 'integer',
+                                    ),
+                                    new OA\Property(
+                                        property: 'author',
+                                        type: 'string',
+                                    ),
+                                    new OA\Property(
+                                        property: 'text',
+                                        type: 'string',
+                                    ),
+                                    new OA\Property(
+                                        property: 'createdAt',
+                                        type: 'string',
+                                        pattern: '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',
+                                    ),
+                                ],
+                                type: 'object'
+                            ),
+                        ),
+                    ],
+                    type: 'object'
+                ),
+            ),
+            new OA\Response(
+                response: 400,
+                description: 'Ошибка валидации',
+            ),
+        ],
+    )]
     #[Route(path: 'api/v1/get-feed/{id}', methods: ['GET'])]
     public function __invoke(#[MapEntity(id: 'id')]User $user, #[MapQueryParameter]?int $count = null): Response
     {
