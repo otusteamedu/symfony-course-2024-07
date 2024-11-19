dade61ff4762f88c27ac698ad95a134a6c1400dc
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-11-19 18:45:55 +0300

    Move statsdBundle to separate repository and add Symfony Flex recipe

--- statsdBundle/src/Storage/MetricsStorageInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace StatsdBundle\Storage;
-
-interface MetricsStorageInterface
-{
-    public function increment(string $key, ?float $sampleRate = null, ?array $tags = null): void;
-}
